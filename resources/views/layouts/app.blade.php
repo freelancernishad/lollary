98f0cdbd577ce0aeb25083d433877c4c7c4ddcc4
Softlabltd.com <freelancernishad123@gmail.com>
2022-12-07 13:34:00 +0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset("js/dataTables.bootstrap5.min.js") }}"></script>
+    <script src="{{ asset("js/jquery.dataTables.min.js") }}"></script>
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
